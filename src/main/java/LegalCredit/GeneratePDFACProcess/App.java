a3efc8e2147e5e5374dab48fd033cb1eac1fc9f9
JhonJaimeMendezAland <JhonJaimeMendezAland@DESKTOP-OF0EC5V>
2021-04-08 13:09:19 -0500

    Estructura del proyecto que recibe un JSON y devuelve un base de un PDF con el resumen de las cuentas a disputas y en estados satisfactorio.

---
+++ src/main/java/LegalCredit/GeneratePDFACProcess/App.java
@@ -0,0 +1,13 @@
+package LegalCredit.GeneratePDFACProcess;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
